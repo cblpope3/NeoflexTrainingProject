17359d64a30083c572ea575fdb1d186634b0b2cb
Nickolay Leonov <nickolayshmakolay@gmail.com>
2021-10-27 15:46:11 +0300

    Point 2. Project initialized from start.spring.io.

---
+++ src/main/java/ru/leonov/neotraining/NeoflexTrainingProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.leonov.neotraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoflexTrainingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoflexTrainingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/leonov/neotraining/NeoflexTrainingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.leonov.neotraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexTrainingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
